62beed42b5397db4fb31ba73d078ef69d4962322
arklow.sei <ru.hickson@gmail.com>
2025-05-12 10:25:01 +0100

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,21 +1,21 @@
 const projects = [
     {
         id: 1,
-        icon: "/public/images/ds.png",
+        icon: "https://github.com/ruhickson/ruhickson/blob/main/public/images/ds.png?raw=true",
         title: "Downsell",
         subTitle: "Full-stack Money Management App",
         link: "https://downsell.netlify.app/",
     },
     {
         id: 2,
-        icon: "/public/images/gitball.png",
+        icon: "https://github.com/ruhickson/ruhickson/blob/main/public/images/gitball.png?raw=true",
         title: "Gitball",
         subTitle: "Pipeline for Alternative Fantasy Football App",
         link: "https://gitball.netlify.app/",
     },
     {
         id: 3,
-        icon: "/public/images/dfaw.png",
+        icon: "https://github.com/ruhickson/ruhickson/blob/main/public/images/dfaw.png?raw=true",
         title: "Fuaime",
         subTitle: "Video Game Production (Coming Soon)",
         link: "#",
