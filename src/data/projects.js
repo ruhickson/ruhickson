5626e18adce7e660cd9c0b1c32dab2abc52b5cdc
arklow.sei <ru.hickson@gmail.com>
2025-05-14 17:13:51 +0100

    Added in extra projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -20,6 +20,27 @@ const projects = [
         subTitle: "Video Game Production (Coming Soon)",
         link: "#",
     },
+    {
+        id: 4,
+        icon: "/images/ww1wicklow.png",
+        title: "W.W.1 Wicklow",
+        subTitle: "Historical Website",
+        link: "https://ww1wicklow.bitbucket.io/",
+    },
+    {
+        id: 5,
+        icon: "/images/tlmtr.png",
+        title: "Telemeter",
+        subTitle: "$SEI Cryptocurrency Portfolio Tracker",
+        link: "https://tlmtr.xyz/",
+    },
+    {
+        id: 6,
+        icon: "/images/arkvision.png",
+        title: "Arkvision",
+        subTitle: "Visual Impairment Simulator",
+        link: "https://arkvision.netlify.app",
+    },
 ];
 
 export default projects;
